ca6d473453484fb302cae4a559685db7bae555a3
Keerthanaa LN <118036652+KeerthanaaLN@users.noreply.github.com>
2025-05-22 15:14:08 +0530

    Create JavaSubarray.java

---
+++ JavaSubarray.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaSubarray {
+
+    public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    Scanner sc = new Scanner(System.in);
+
+    //Read array size
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+
+    //Read array element 
+    for (int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+
+            if(sum < 0){
+                count++;
+            }
+        }
+    }
+    System.out.println(count);
+}   
+}
