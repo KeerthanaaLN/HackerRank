bc164472503361f6192fbbb00d57bfbc60869075
Keerthanaa LN <118036652+KeerthanaaLN@users.noreply.github.com>
2025-05-22 17:17:09 +0530

    Create JavaInterface.java

---
+++ JavaInterface.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n){
+        int sum = 0;
+        
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
+
+public class JavaInterface {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       Scanner input = new Scanner(System.in);
+       int n = input.nextInt();
+       
+       AdvancedArithmetic calculator = new MyCalculator(); 
+       System.out.println("I implemented: AdvancedArithmetic");
+       System.out.println(calculator.divisor_sum(n));
+    }
+}
+
