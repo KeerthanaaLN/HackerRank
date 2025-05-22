1ead1726c4624686997454fcaea86e1c2e11dce1
Keerthanaa LN <118036652+KeerthanaaLN@users.noreply.github.com>
2025-05-22 15:53:06 +0530

    Create JavaGenerics.java

---
+++ JavaGenerics.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaGenerics {
+    private static <T> void printArray(T[] array) {
+        for(T item: array)
+            System.out.println(item);
+    }
+
+    public static void main(String[] args) {
+        printArray(new Integer[]{1, 2, 3});
+        printArray(new String[]{"Hello", "World"});
+    }
+}
