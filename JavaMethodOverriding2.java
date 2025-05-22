b4d1c18debc83036fdb0786870e6a6db691e3a66
Keerthanaa LN <118036652+KeerthanaaLN@users.noreply.github.com>
2025-05-22 17:42:24 +0530

    Create JavaMethodOverriding2.java

---
+++ JavaMethodOverriding2.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+class BiCycle
+{
+   String define_me()
+   {
+      return "a vehicle with pedals.";
+   }
+}
+
+class MotorCycle extends BiCycle
+{
+   String define_me()
+   {
+      return "a cycle with an engine.";
+   }
+   
+   MotorCycle()
+   {
+      
+      
+      System.out.println("Hello I am a motorcycle, I am "+ define_me());
+      String temp=super.define_me();
+      System.out.println("My ancestor is a cycle who is "+ temp );
+   }
+   
+} 
+
+public class JavaMethodOverriding2 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        MotorCycle M=new MotorCycle();
+    }
+}
